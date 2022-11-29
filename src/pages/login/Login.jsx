0d3dce444eddb92f677735cacc88771457d06b53
Pratik Dhungel <pratikdhungel7.0@gmail.com>
2022-11-29 11:37:40 +0545

    Feature: Setup login page and login form component

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import './login.css';
+import appLogo from '../../assets/docsumoLogo.png';
+import { LoginForm } from '../../components';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className='login-wrapper'>
+      <div className='form-container'>
+        <div className='app-logo'>
+          <img src={appLogo} />
+        </div>
+
+        <div className='login-form-container'>
+          <h1 className='login-form-title'>Login to your Docsumo account</h1>
+
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
